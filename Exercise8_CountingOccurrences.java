2b32cacd35357e53066837fb85f21094130cbcb5
jwheller0013 <jwheller0013@yahoo.com>
2025-02-26 14:03:00 -0500

    Ex8 Complete

--- Exercise8_CountingOccurrences.java
+++ Exercise8_CountingOccurrences.java
@@ -4,6 +4,7 @@
  * Write a program that asks the user for a string and a character,
  * then counts how many times that character appears in the string.
  */
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise8_CountingOccurrences {
@@ -16,8 +17,17 @@ public class Exercise8_CountingOccurrences {
         System.out.print("Enter a character to count: ");
         char charToCount = scanner.nextLine().charAt(0);
         
-        // TODO: Count and print the number of occurrences of the character
-        
+        // TODO: Count,and print the number of occurrences of the character
+
+        String inputa= input.toLowerCase(); //to account for first letter of sentance being capital
+        int count = 0;
+        for (int i=0; i<inputa.length(); i++){
+            if (inputa.charAt(i) == charToCount){
+                count++;
+            }
+        }
+        System.out.print(count + " instances of \"character\".");
+
         scanner.close();
     }
 }
