de6b9a2f4ecbc492467e47b116b4d0247f1289eb
jwheller0013 <jwheller0013@yahoo.com>
2025-02-26 11:59:34 -0500

    Ex1 Complete

--- Exercise1_StringLength.java
+++ Exercise1_StringLength.java
@@ -12,10 +12,10 @@ import java.util.Scanner;
 public class Exercise1_StringLength {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Please enter your name: ");
+
+        System.out.print("Please enter your name:");
         String name = scanner.nextLine();
-        
+        System.out.print(name.length());
         // TODO: Print the length of the name using the length() method
         
         scanner.close();
